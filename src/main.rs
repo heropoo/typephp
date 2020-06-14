b677fc7f5cbe4aa4228b40185dc50dd5761a85b6
heropoo <aiyouyou1000@163.com>
2020-06-14 11:55:57 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+
 fn main() {
     println!("Hello, world!");
+    let output = Command::new("git")
+        .arg("log|head -n 1|awk '{printf $2}'")
+        .output()
+        .expect("git log command failed to run");
+
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    assert!(output.status.success());
 }
